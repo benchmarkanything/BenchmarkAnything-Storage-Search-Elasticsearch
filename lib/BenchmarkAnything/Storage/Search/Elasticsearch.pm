d54339a2d6b1058981c82f8ebf41b3c7d3305591
Steffen Schwigon <ss5@renormalist.net>
2017-01-30 10:18:33 +0100

    initial commit

---
+++ lib/BenchmarkAnything/Storage/Search/Elasticsearch.pm
@@ -0,0 +1,7 @@
+use 5.008;
+use strict;
+use warnings;
+package BenchmarkAnything::Storage::Search::Elasticsearch;
+# ABSTRACT: Utility functions to use Elasticsearch with BenchmarkAnything storage
+
+1;
